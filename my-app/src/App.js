d5c5bc67d2f547b45ac14353cef71bcb18b7d131
apple <apple@appledeMacBook-Pro.local>
2019-02-15 00:19:50 +0800

    add get test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,28 +2,59 @@ import React, { Component } from 'react';
 import './App.css';
 import $ from  'jquery'
 
+// eslint-disable-next-line
+function ListItem(props){
+    console.log(3);
+    console.log(props.value);
+    return <li>{props.value.description}</li>;
+}
+
 class App extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            data:{
+                "count":0,
+                "next":null,
+                "previous":null,
+                "results":[],
+            }
+        }
+    }
+
     componentDidMount() {
+        var data_tmp=null;
         $.ajax({
             url: "http://127.0.0.1:8000/api/tasks/",
             dataType: 'json',
             headers:{"Authorization":"Basic bHg6bHg4NTA5MDA2Ng=="},
             type: 'get',
             async:false,
-            success: function(data) {
-                console.log(data);
+            success: function(data,textStatus){
+               data_tmp=data;
             },
              error: function(xhr) {
             alert(xhr.status+" "+xhr.readyState+" "+xhr.statusText);
             },
         });
+        this.setState(function(){
+            console.log(2);
+            console.log(data_tmp);
+           return {data:data_tmp};
+        });
     }
+
     render() {
         return (
-             <div>
-                 {data.result}
-             </div>
+            <ul>
+                {console.log(1)}
+                {console.log(this.state.data)}
+                {this.state.data.results.map((item)=>
+                    <ListItem key={item.id}
+                              value={item}/>
+                )}
+            </ul>
         );
     }
 
